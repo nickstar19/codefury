1107cbe58dbe60ab67a7848236056ce9e469fd8a
Nikhil Patil <nikhil.patil_entc21@pccoer.in>
2024-08-11 06:05:16 +0530

    production build1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
